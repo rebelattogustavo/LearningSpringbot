faab9695a04ce90bde118f1c21e5106f08871274
rebelattogustavo <gu.zapella@gmail.com>
2022-11-16 21:49:36 -0300

    feat: added the editora livros teste entitiy

--- editora-livros/src/test/java/br/senai/sc/editoralivros/EditoraLivrosApplicationTests.java
+++ editora-livros/src/test/java/br/senai/sc/editoralivros/EditoraLivrosApplicationTests.java
@@ -3,11 +3,19 @@ package br.senai.sc.editoralivros;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 @SpringBootTest
 class EditoraLivrosApplicationTests {
 
 	@Test
 	void contextLoads() {
+		try {
+			Optional optional = Optional.ofNullable(null);
+			System.out.println(optional.get());
+		}catch (Exception e){
+			System.out.println(e.getLocalizedMessage());
+		}
 	}
 
 }
